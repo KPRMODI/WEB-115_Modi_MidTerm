a0c5d8e67dfad9559bbe0c15efce6ba15d8809d9
KPRMODI <kpmodi@my.waketech.edu>
2025-04-03 12:28:42 -0400

    added

--- codingExam.js
+++ codingExam.js
@@ -5,32 +5,44 @@ button1.addEventListener("click", function(){
     let loanAmount = window.prompt("Enter loan amount: ")
     let downPayment = window.prompt("Enter down payment as a percentage (0-100): ")
     let downpaymentamount = loanAmount * (downPayment / 100)
-    let interestRate = 0.07
+    let interestRate = 0.0575
     let termYears = window.prompt("Enter term years (30 or 15)")
 
+    let PrincipalLoanAmount = loanAmount - (downPayment * loanAmount)
+    PrincipalLoanAmount1 = document.createElement("p")
+    PrincipalLoanAmount1.textContent = "principle loan amount " + PrincipalLoanAmount
+    document.getElementById("finance").appendChild(PrincipalLoanAmount1)
+
+    interestRate1 = document.createElement("p")
+
     let monthlypayment = (((interestRate / 12) * loanAmount) / (1 - Math.pow(1 + (interestRate / 12), (termYears * -12)))) 
+
     monthlypaymentshow = document.createElement("p")
     monthlypaymentshow.textContent = "Monthly Payment each month " + monthlypayment
-    document.getElementById("lol").appendChild(monthlypaymentshow)
+    document.getElementById("finance").appendChild(monthlypaymentshow)
 
     let totalInterest = (monthlypayment * termYears * 12) - loanAmount
     totalInterestshow = document.createElement("p")
     totalInterestshow.textContent = "total interest each month " + totalInterest
-    document.getElementById("lol").appendChild(totalInterestshow)
+    document.getElementById("finance").appendChild(totalInterestshow)
 
     let mortageamount = loanAmount + totalInterest
     mortageamountshow = document.createElement("p")
     mortageamountshow.textContent = "mortage amount each month " + mortageamount
-    document.getElementById("lol").appendChild(mortageamountshow)
+    document.getElementById("finance").appendChild(mortageamountshow)
 
     let Monthlyloanbalance = mortageamount - monthlypayment
     monthlyloanbalanceshow = document.createElement("p")
-    monthlyloanbalanceshow.textContent = "monthly loan balance each month " + Monthlyloanbalance
-    document.getElementById("lol").appendChild(monthlyloanbalanceshow)
+    monthlyloanbalanceshow.textContent = "monthly loan balance each month " + Monthlyloanbalance.toFixed(2)
+    document.getElementById("finance").appendChild(monthlyloanbalanceshow)
 
+    i = 1;
     while (loanAmount > 0){
         loanAmount -= monthlypayment
-        loanamountshow = document.createElement()
+        loanamountshow = document.createElement("p")
+        loanamountshow.textContent = "Month "+ i+" loan balance each month :" + loanAmount.toFixed(2)
+        document.getElementById("finance").appendChild(loanamountshow)
+        i ++
     }
 
 })
